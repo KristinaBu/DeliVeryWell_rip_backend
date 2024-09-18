530493f1d916795650471401432fb49787603be0
KristinaBu <krisdinabui@gmail.com>
2024-09-18 12:10:26 +0300

    lab1 1(minio)

--- internal/transport/routes.go
+++ internal/transport/routes.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func InitializeRouters(router *gin.Engine) {
 	router.GET("/", showIndexPage)
 
-	router.GET("/card/:card_id", getCallCard)
+	router.GET("/aboutcall/:card_id", getCallCard)
 	router.GET("/mycalls", getMyCallCards)
 
 }
